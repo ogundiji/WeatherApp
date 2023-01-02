8934a4cb0832368284774e4726c13ab3352b8b4d
Yusuf Ogundiji <Yusuf.Ogundiji@xpresspayments.com>
2023-01-02 05:02:20 +0100

    design the weather ui based on the figma design

---
+++ WeatherApp/WeatherApp/Models/Weather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public string Temp { get; set; }
+        public string Date { get; set; }
+        public string Icon { get; set; }
+    }
+}
